744af4618161dc0eb6a0c7121d6f5369dc6f1fcd
kleber <klcabarite@gmail.com>
2021-02-15 20:19:49 -0300

    update to make person and employer abstratc class

---
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Funcionario.kt
@@ -0,0 +1,11 @@
+package one.digitalinnovation.digionebank
+
+import java.math.BigDecimal
+
+abstract class Funcionario(
+    name: String,
+    cpf: String,
+    val salario: BigDecimal
+): Pessoa(name, cpf) {
+    abstract fun calculoAuxilio()
+}
--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,31 +1,40 @@
 package one.digitalinnovation.digionebank
 
 // Classe Pessoa
-class Pessoa {
-    var nome: String = "Kleber"
+abstract class Pessoa (
+    val name: String,
+    val cpf: String
+)
 
-    var cpf: String = "310.555.398-32"
-    private set // private set nao permite que mexa em qualquer lugar no projeto
 
-    constructor()
 
-    fun personInfo() = "$nome = $cpf"
+
+
+
+ //   var nome: String = "Kleber"
+
+   // var cpf: String = "310.555.398-32"
+    //private set // private set nao permite que mexa em qualquer lugar no projeto
+
+   // constructor()
+
+    //fun personInfo() = "$nome = $cpf"
 
 // Criando uma classe dentro da outra (inner class)
 //    inner class endereco {
 //        var rua: String = "Rua teste"
 
  //   }
-}
-    fun main(){
-        val kleber = Pessoa()
+//}
+    //fun main(){
+ //       val kleber = Pessoa()
 
      //   println(kleber)
-        println(kleber.personInfo())
+       // println(kleber.personInfo())
         //println(kleber.nome)
         // println(kleber.cpf)
 
 //        println(kleber.endereco().rua)
 
-        }
+//        }
 
