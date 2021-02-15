ea0f12738df98dd988b8f9a7f05d4daaaf8a20cf
kleber <klcabarite@gmail.com>
2021-02-15 12:59:29 -0300

    created person class

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -1,7 +1,14 @@
 package one.digitalinnovation.digionebank
 
-abstract class Pessoa(
-    val nome: String,
-    val cpf: String
-)
+class Pessoa {
+    var nome: String = "Kleber"
+    var cpf: String = "310.555.398-32"
+}
+    fun main(){
+        val kleber = Pessoa()
+
+        println(kleber.nome)
+        println(kleber.cpf)
+
+        }
 
