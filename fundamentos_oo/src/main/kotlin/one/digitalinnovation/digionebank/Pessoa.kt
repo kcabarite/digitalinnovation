6f3df7a435b04f7b6c71325f6da24b24b8ab3a0f
kleber <klcabarite@gmail.com>
2021-02-15 13:54:14 -0300

    update to talk about class members

--- fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
+++ fundamentos_oo/src/main/kotlin/one/digitalinnovation/digionebank/Pessoa.kt
@@ -7,6 +7,10 @@ class Pessoa {
     var cpf: String = "310.555.398-32"
     private set // private set nao permite que mexa em qualquer lugar no projeto
 
+    constructor()
+
+    fun personInfo() = "$nome = $cpf"
+
 // Criando uma classe dentro da outra (inner class)
 //    inner class endereco {
 //        var rua: String = "Rua teste"
@@ -16,10 +20,10 @@ class Pessoa {
     fun main(){
         val kleber = Pessoa()
 
-
-        println(kleber)
-        println(kleber.nome)
-        println(kleber.cpf)
+     //   println(kleber)
+        println(kleber.personInfo())
+        //println(kleber.nome)
+        // println(kleber.cpf)
 
 //        println(kleber.endereco().rua)
 
